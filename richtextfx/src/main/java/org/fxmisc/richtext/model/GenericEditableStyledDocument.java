1a6d956c2fba90d700910eb246f8ab4d2721a60f
Jordan Martinez <jordanalex.martinez@gmail.com>
2016-10-20 08:39:48 -0700

    Add javadoc to two implementations of ESD

--- richtextfx/src/main/java/org/fxmisc/richtext/model/GenericEditableStyledDocument.java
+++ richtextfx/src/main/java/org/fxmisc/richtext/model/GenericEditableStyledDocument.java
@@ -1,5 +1,12 @@
 package org.fxmisc.richtext.model;
 
+/**
+ * Provides a basic implementation of {@link EditableStyledDocument}. See {@link SimpleEditableStyledDocument} for
+ * a version that is specified for {@link StyledText}.
+ * @param <PS>
+ * @param <SEG>
+ * @param <S>
+ */
 public final class GenericEditableStyledDocument<PS, SEG, S> extends GenericEditableStyledDocumentBase<PS, SEG, S>
         implements EditableStyledDocument<PS, SEG, S> {
 
--- richtextfx/src/main/java/org/fxmisc/richtext/model/SimpleEditableStyledDocument.java
+++ richtextfx/src/main/java/org/fxmisc/richtext/model/SimpleEditableStyledDocument.java
@@ -1,5 +1,9 @@
 package org.fxmisc.richtext.model;
 
+/**
+ * Provides an implementation of {@link EditableStyledDocument} that is specified for {@link StyledText} as its segment.
+ * See also {@link GenericEditableStyledDocument}.
+ */
 public final class SimpleEditableStyledDocument<PS, S> extends GenericEditableStyledDocumentBase<PS, StyledText<S>, S> {
 
     public SimpleEditableStyledDocument(PS initialParagraphStyle, S initialStyle) {
