072b42c667163beb73815b766587b1da23cbca3d
Andreas Fester <Andreas.Fester@gmx.de>
2017-09-27 15:51:13 +0200

    Added underline test (currently failing)

--- richtextfx/src/integrationTest/java/org/fxmisc/richtext/style/StylingTests.java
+++ richtextfx/src/integrationTest/java/org/fxmisc/richtext/style/StylingTests.java
@@ -2,6 +2,9 @@ package org.fxmisc.richtext.style;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import org.fxmisc.flowless.Cell;
@@ -14,6 +17,7 @@ import com.nitorcreations.junit.runners.NestedRunner;
 
 import javafx.scene.Node;
 import javafx.scene.layout.Region;
+import javafx.scene.shape.Path;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
@@ -34,7 +38,25 @@ public class StylingTests extends InlineCssTextAreaAppTest {
         Cell gsa = (Cell) flow.getCell(0);
         Region pgb = (Region) gsa.getNode();
         TextFlow tf = (TextFlow) pgb.getChildrenUnmodifiable().get(0);
+        
+        // get the ParagraphText (protected subclass of TextFlow) 
         return tf.getChildrenUnmodifiable().filtered(n -> n instanceof Text);
+    }
+
+    /**
+     * @return A list of nodes which render the underlines for the current text.
+     */
+    private List<Path> getUnderlinePaths() {
+        VirtualFlow flow = (VirtualFlow) area.getChildrenUnmodifiable().get(0);
+        Cell gsa = (Cell) flow.getCell(0);
+        Region pgb = (Region) gsa.getNode();
+        
+        // get the ParagraphText (protected subclass of TextFlow) 
+        TextFlow tf = (TextFlow) pgb.getChildrenUnmodifiable().get(0);
+
+        List<Path> result = new ArrayList<>();
+        tf.getChildrenUnmodifiable().filtered(n -> n instanceof Path).forEach(n -> result.add((Path) n));
+        return result.subList(2, result.size());
     }
 
 
@@ -49,9 +71,8 @@ public class StylingTests extends InlineCssTextAreaAppTest {
         List<Node> textNodes = getTextNodes();
         assertEquals(1, textNodes.size());
 
-        // select the word "World" and set it to bold
+        // Set word "World" to bold
         interact(() -> {
-            area.selectRange(firstWord.length(), firstWord.length() + styledWord.length());
             area.setStyle(firstWord.length(), firstWord.length() + styledWord.length(), "-fx-font-weight: bold;");
         });
 
@@ -60,11 +81,16 @@ public class StylingTests extends InlineCssTextAreaAppTest {
         assertEquals(3, textNodes.size());
 
         Text first = (Text) textNodes.get(0);
-        Text second = (Text) textNodes.get(1);
-        Text third = (Text) textNodes.get(2);
         assertEquals("Hello ", first.getText());
+        assertEquals("Regular", first.getFont().getStyle());
+
+        Text second = (Text) textNodes.get(1);
         assertEquals("World", second.getText());
+        assertEquals("Bold", second.getFont().getStyle());
+
+        Text third = (Text) textNodes.get(2);
         assertEquals(" and Moon", third.getText());
+        assertEquals("Regular", third.getFont().getStyle());
     }
 
 
@@ -73,34 +99,48 @@ public class StylingTests extends InlineCssTextAreaAppTest {
         // setup
         interact(() -> {
             area.replaceText(firstWord + styledWord + moreText + styledWord2 + remainingLine);
+            //               "Hello World and also the Sun and Moon"
         });
 
         // expected: one text node which contains the complete text
         List<Node> textNodes = getTextNodes();
         assertEquals(1, textNodes.size());
 
-        // select the word "World" and set it to bold
         interact(() -> {
-            //area.selectRange(firstWord.length(), firstWord.length() + styledWord.length());
             area.setStyle(firstWord.length(), firstWord.length() + styledWord.length(),
                           "-rtfx-underline-color: red; -rtfx-underline-dash-array: 2 2; -rtfx-underline-width: 1; -rtfx-underline-cap: butt;");
 
-            // area.selectRange(26, 28); // firstWord.length(), firstWord.length() + styledWord.length());
-            area.setStyle(26, 28, // firstWord.length(), firstWord.length() + styledWord.length(),
+            area.setStyle(25, 28, // firstWord.length(), firstWord.length() + styledWord.length(),
                           "-rtfx-underline-color: red; -rtfx-underline-dash-array: 2 2; -rtfx-underline-width: 1; -rtfx-underline-cap: butt;");
+
+            //               "Hello World and also the Sun and Moon"
+            //                      ^^^^^              ^^^
         });
 
-        sleep(5000);
-        // expected: three text nodes
+        // expected: five text nodes
         textNodes = getTextNodes();
-        assertEquals(3, textNodes.size());
+        assertEquals(5, textNodes.size());
 
         Text first = (Text) textNodes.get(0);
-        Text second = (Text) textNodes.get(1);
-        Text third = (Text) textNodes.get(2);
         assertEquals("Hello ", first.getText());
+
+        Text second = (Text) textNodes.get(1);
         assertEquals("World", second.getText());
-        assertEquals(" and Moon", third.getText());
+
+        Text third = (Text) textNodes.get(2);
+        assertEquals(" and also the ", third.getText());
+
+        Text fourth = (Text) textNodes.get(3);
+        assertEquals("Sun", fourth.getText());
+
+        Text fifth = (Text) textNodes.get(4);
+        assertEquals(" and Moon", fifth.getText());
+
+        // determine the underline paths
+        List<Path> underlineNodes = getUnderlinePaths();
+        assertEquals(2, underlineNodes.size());
+
+        underlineNodes.forEach(e -> System.err.println(e));
     }
 
 }
